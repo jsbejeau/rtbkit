46afac991056f45e3956be8f9bce3c192c0d0617
jsbejeau <bejeau.js@gmail.com>
2013-06-22 15:51:22 -0400

    Add bit_request support for FBX

--- plugins/bid_request/bid_request.mk
+++ plugins/bid_request/bid_request.mk
@@ -5,6 +5,7 @@
 # Makefile for bid request deserializers and parsers
 
 $(eval $(call library,openrtb_bid_request,openrtb_bid_request.cc,bid_request openrtb))
+$(eval $(call library,fbx_bid_request,fbx_bid_request.cc,bid_request fbx))
 
 $(eval $(call include_sub_make,bid_request_testing,testing,bid_request_testing.mk))
 
